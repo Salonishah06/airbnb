2bebf0739581cb16591f3ea50c60d9d6deac6ccd
Saloni <salonishah306@gmail.com>
2023-02-22 22:14:17 +0530

    header left side done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>RentAPlace</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      
+      {/* <h1>Hello World</h1> */}
+        {/* Header */}
+        <Header/>
+        {/* Banner */}
     </div>
   )
 }
