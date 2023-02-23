57ff0accb7afe4928fafa93535ff3bcb4b3a4cc2
Saloni <salonishah306@gmail.com>
2023-02-23 20:47:07 +0530

    Banner done

---
+++ components/Banner.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Banner = () => {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+      <Image src="https://links.papareact.com/0fm" alt="Banner" layout='fill'
+        objectFit='cover' />
+      <div className="absolute w-full top-1/2 text-center">
+        <p className="text-sm sm:txt-lg font-bold">Not sure Where to go?Perfect!!!</p>
+
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-lg active:scale-90 transition duration-150">I'm Flexible</button>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Head>
-        <title>RentAPlace</title>
+        <title>AirBnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* <h1>Hello World</h1> */}
         {/* Header */}
         <Header/>
         {/* Banner */}
+        <Banner/>
     </div>
   )
 }
